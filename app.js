a2bb31b37dd77461296b80182b123abd3c7689b5
Vmohan5072 <vmohan5072@gmail.com>
2024-08-23 22:02:47 -0400

    reworked main page

--- app.js
+++ app.js
@@ -1,70 +1,15 @@
+// Load dotenv and discordjs classes
 import 'dotenv/config';
-import express from 'express';
-import {
-  InteractionType,
-  InteractionResponseType,
-  verifyKeyMiddleware,
-} from 'discord-interactions';
-import { getRandomEmoji } from './utils.js';
+import { Client, Events, GatewayIntentBits } from 'discord.js';
 
-// Create an express app
-const app = express();
-// Get port, or default to 3000
-const PORT = process.env.PORT || 3000;
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-/**
- * Interactions endpoint URL where Discord will send HTTP requests
- * Parse request body and verifies incoming requests using discord-interactions package
- */
-app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async function (req, res) {
-  // Interaction type and data
-  const { type, data } = req.body;
+// Fetch token from env and logs in
+const discToken = process.env.DISCORD_TOKEN;
+client.login(discToken);
 
-  /**
-   * Handle verification requests
-   */
-  if (type === InteractionType.PING) {
-    return res.send({ type: InteractionResponseType.PONG });
-  }
-
-  /**
-   * Handle slash command requests
-   * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
-   */
-  if (type === InteractionType.APPLICATION_COMMAND) {
-    const { name } = data;
-
-    // "test" command
-    if (name === 'test') {
-      // Send a message into the channel where command was triggered from
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          // Fetches a random emoji to send from a helper function
-          content: `hello world! ${getRandomEmoji()}`,
-        },
-      });
-    }
-    
-    if (name === 'testing') {
-      // Send a message into the channel where command was triggered from
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          // Fetches a random emoji to send from a helper function
-          content: `hello mom ${getRandomEmoji()}`,
-        },
-      });
-    }
-
-    console.error(`unknown command: ${name}`);
-    return res.status(400).json({ error: 'unknown command' });
-  }
-
-  console.error('unknown interaction type', type);
-  return res.status(400).json({ error: 'unknown interaction type' });
-});
-
-app.listen(PORT, () => {
-  console.log('Listening on port', PORT);
+// When the client is connects, print success message
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
--- commands/base-commands/ping.js
+++ commands/base-commands/ping.js
@@ -8,3 +8,28 @@ module.exports = {
 		await interaction.reply('Pong!');
 	},
 };
+
+/*TODO: add the following back into index.js
+// "test" command
+if (name === 'test') {
+	// Send a message into the channel where command was triggered from
+	return res.send({
+	  type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+	  data: {
+		// Fetches a random emoji to send from a helper function
+		content: `hello world! ${getRandomEmoji()}`,
+	  },
+	});
+  }
+  
+  if (name === 'testing') {
+	// Send a message into the channel where command was triggered from
+	return res.send({
+	  type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+	  data: {
+		// Fetches a random emoji to send from a helper function
+		content: `hello mom ${getRandomEmoji()}`,
+	  },
+	});
+  }
+*/
