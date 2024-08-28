9db7a1ba881b914e74e970031f80eadf5b424933
Vmohan5072 <vmohan5072@gmail.com>
2024-08-28 19:26:28 -0400

    Moderation command folder created

---
+++ commands/moderation/kick.js
@@ -0,0 +1,10 @@
+//Kicks a user
+import { SlashCommandBuilder } from '@discordjs/builders';
+
+export const data = new SlashCommandBuilder()
+	.setName('kick')
+	.setDescription('Kicks a user from the server');
+
+export async function execute(interaction) {
+	await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`);
+}
